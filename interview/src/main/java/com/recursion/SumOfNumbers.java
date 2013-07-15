39a2ce53b91af9df671c1e25fb2838faacba64e7
skandula <skandula@srinivas-laptop.com>
2013-07-15 11:48:50 +0530

    Recursive version.

--- interview/src/main/java/com/recursion/SumOfNumbers.java
+++ interview/src/main/java/com/recursion/SumOfNumbers.java
@@ -3,7 +3,7 @@ package com.recursion;
 //Finding sum of numbers recursively
 public class SumOfNumbers {
 	public static void main(String args[]) {
-		System.out.println("The sum of numbers " + sumOfNumbers(1098765));
+		System.out.println("The sum of induvidual numbers " + sumOfNumbers(1098765));
 	}
 	
 	public static int sumOfNumbers(int num) {
